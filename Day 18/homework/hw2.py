375308b39c8b50aaf89ceaa8955e8e6ffdfa3d71
anastasia171 <anastasiakhakhiauri@gmail.com>
2024-12-10 23:46:46 +0400

    Add files via upload

---
+++ Day 18/homework/hw2.py
@@ -0,0 +1,8 @@
+# 2) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი შეამოწმეთ ეს რიცხვი არის თუ არა ხუთის ჯერადი
+
+num = int(input("enter number: "))
+
+if num % 5 == 0:
+    print("xutis jeradia")
+else:
+    print("araa xutis jeradi")
---
+++ Day 18/homework/hw3.py
@@ -0,0 +1,8 @@
+num = int(input("enter number: "))
+
+if num > 0:
+    print("dadebitia")
+elif num < 0:
+    print("uaryofitia")
+else:
+    print(int(input("enter number: ")))
